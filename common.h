ac4090f8b98a166face12899a80f10f3f6dee3f1
sih <ladokihosaha@gmail.com>
2024-05-03 08:07:11 +0100

    Added more explanations to the debug flags in the common.h

--- common.h
+++ common.h
@@ -5,9 +5,13 @@
 #include <stddef.h>
 #include <stdint.h>
 
-// #define DEBUG_PRINT_CODE
-// Uncomment this to toggle off debug mode
-// #define DEBUG_TRACE_EXECUTION
+// Comment this line to avoid dumping the whole content of the chunk bytecode
+#define DEBUG_PRINT_CODE
+
+/* When this flag is defined, the VM disassembles and prints each instruction
+right before executing it. Uncomment this to toggle off debug mode
+comment it to avoid this behaviour*/
+#define DEBUG_TRACE_EXECUTION
 
 #define UINT8_COUNT (UINT8_MAX + 1)
 
